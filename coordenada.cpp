933ce365b90b9742c6815334a2945acc22ab626e
Rennosmx <rennosmx@hotmail.com>
2015-05-14 11:37:49 -0300

    Criação de classe Coordenada

---
+++ coordenada.cpp
@@ -0,0 +1,26 @@
+#include "coordenada.h"
+
+Coordenada::Coordenada()
+{
+
+}
+
+int Coordenada::getX(){
+
+    return x;
+}
+
+int Coordenada::getY(){
+
+    return y;
+}
+
+void Coordenada::setX(int x){
+
+    this->x = x;
+}
+
+void Coordenada::setY(int y){
+
+    this->y = y;
+}
